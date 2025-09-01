9ccc2d397a7ea51d51df3a5221fed70be1ecfd59
nishikou <nkook.1024@gmaill.com>
2025-09-01 23:37:30 +0900

    追加

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-alert("Hello World!");
+alert("Hello World!");// Hello,Worldと表示する命令文
+
+alert("Hello");alert("World");//1行でも書ける　1行で書く場合は；（セミコロン）←必須
+
+alert("Hello");
+alert("World");//改行でもok
+
+alert("Hello")
+alert("World")//；←なくても実行できるが一部のケースで省略するとバグになる
+
+//セミコロンが入らないケース
+alert(3+
+    1
+    +2); //→６を出力
+
+//配列や括弧()[]の直前
+alert("There will be an error")
+[1,2].forEach(alert)//これだと1つの文だと解釈してエラーになる
+//→alert("There will be an error")[1,2].forEach(alert)
+
+
+//正しくは
+alert("There will be an error");
+[1,2].forEach(alert)
